4938ec63e9dccdb8eb88effb72bc176f34f53ad6
Chuckz Okoye <tricelex@gmail.com>
2020-05-28 17:49:10 +0100

    added detail, vote and results to polls view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
